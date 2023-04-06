efcb82ecd164ae722a5db090030fc1211df88b7b
Saoirse Siobhan Ebert <ssiobhan.ebert@gmail.com>
2023-04-06 12:21:07 -0400

    change from main to index, add SearchBar and ResultsPage

---
+++ src/ResultsPage/ResultsPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { SearchBar } from '../SearchBar/SearchBar.jsx';
+
+const ResultsPage = () => {
+    return (
+        <div>
+            <SearchBar/>
+        </div>
+    )
+}
+
+export default ResultsPage
