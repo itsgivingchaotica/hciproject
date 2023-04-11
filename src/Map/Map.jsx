628f7861fdf4652721427f684e7791485f2e96b0
Saoirse Siobhan Ebert <ssiobhan.ebert@gmail.com>
2023-04-11 19:52:03 -0400

    rearrange google api key to .env file and center map

--- src/Map/Map.jsx
+++ src/Map/Map.jsx
@@ -1,20 +1,20 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLocationDot } from '@fortawesome/free-solid-svg-icons'
-import { GoogleMap, Marker } from '@react-google-maps/api'
+import { GoogleMap, MarkerF } from '@react-google-maps/api'
 import styles from './map.module.css'
 
 export function Map({latitude,longitude}){
     // const center = useMemo(() => )
     // latitude = parseFloat(latitude);
     // longitude = parseFloat(longitude);
-    console.log(latitude);
-    console.log(longitude);
+    const center = useMemo(() => ({lat:latitude,lng:longitude}),[])
+    console.log(center);
     return (
         <div className={styles.width}>
-            <GoogleMap zoom={10} center={{lat:latitude,lng:longitude}} mapContainerClassName={styles.mapContainer}>
-                <Marker></Marker>
+            <GoogleMap zoom={13} center={center} mapContainerClassName={styles.mapContainer}>
+                <MarkerF position={center}></MarkerF>
             </GoogleMap>
         </div>
     )
-  } 
+  }
--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+
+if (import.meta.env.NODE_ENV === 'development') {
+  // Only load dotenv in development mode
+  import('dotenv').then((dotenv) => dotenv.config());
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
