40680f9218d6e6cbcd49ff7d4599712a95cc19a2
Saoirse Siobhan Ebert <ssiobhan.ebert@gmail.com>
2023-04-14 00:08:08 -0400

    api key check

--- src/Map/MapLoader.jsx
+++ src/Map/MapLoader.jsx
@@ -3,6 +3,7 @@ import { GoogleMap, useLoadScript, Marker } from '@react-google-maps/api';
 import { Map } from './Map.jsx'
 
 export function MapLoader({latitude,longitude}){
+
 const { isLoaded } = useLoadScript({ googleMapsApiKey: import.meta.env.VITE_NEXT_PUBLIC_GOOGLE_MAPS_API_KEY})
 console.log(isLoaded);
 
