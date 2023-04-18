d9796d35ad285afcc0fd15391d38c8f6e476ca61
Saoirse Siobhan Ebert <91578619+itsgivingchaotica@users.noreply.github.com>
2023-04-18 01:00:34 -0400

    fixed caroulel bug

--- src/ResultsPage/Results/Result/ResultDetails/ResultDetails.jsx
+++ src/ResultsPage/Results/Result/ResultDetails/ResultDetails.jsx
@@ -9,17 +9,50 @@ import { MapLoader } from "../../../../Map/MapLoader.jsx"
 
 export function ResultDetails(props){
 
-  const { reviewName, photoOne, zipCode,reviewLongitude,reviewLatitude} = props;
+  const { id } = useParams();
 
-  console.log(reviewName + " " + photoOne + " " + zipCode + " " + reviewLatitude + " " + reviewLongitude + " sources");
+  const { result, reviewName, photoOne, photoTwo, photoThree, zipCode,reviewLongitude,reviewLatitude} = props;
+
+  console.log(reviewName + " " + photoOne + " " + photoTwo + " " + photoThree + " " + zipCode + " " + reviewLatitude + " " + reviewLongitude + " sources");
+
+  // Use state to store the photo props
+  const [photoOneUrl, setPhotoOneUrl] = useState(photoOne);
+  const [photoTwoUrl, setPhotoTwoUrl] = useState(photoTwo);
+  const [photoThreeUrl, setPhotoThreeUrl] = useState(photoThree);
+
+  // Use the useEffect hook to reload and reset the photo props
+  useEffect(() => {
+    setPhotoOneUrl(photoOne);
+    setPhotoTwoUrl(photoTwo);
+    setPhotoThreeUrl(photoThree);
+  }, [photoOne, photoTwo, photoThree]);
+
   return (
+
+//     <Carousel.Item key={`${photoOne}-1`} onTouchStart={(e) => {
+//   // Add the passive option to the touchstart event listener
+//   e.preventDefault(); 
+// }} passive={true}>
+//   <img
+//     className={styles.photos}
+//     src={photoOneUrl}
+//     alt="First slide"
+//   />
+//   <Carousel.Caption>
+//     <h3>{reviewName}</h3>
+//     <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+//   </Carousel.Caption>
+// </Carousel.Item>
+
     <div className={styles.resultsDetails}>
     <div >
     <Carousel fade>
-      <Carousel.Item>
+      <Carousel.Item key={`${photoOne}-1`} onTouchStart={(e) => {
+        e.preventDefault(); 
+      }} passive="true">
         <img
           className={styles.photos}
-          src={photoOne}
+          src={photoOneUrl}
           alt="First slide"
         />
         <Carousel.Caption>
@@ -27,22 +60,23 @@ export function ResultDetails(props){
           <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
         </Carousel.Caption>
       </Carousel.Item>
-      <Carousel.Item>
+
+      <Carousel.Item key={`${photoTwo}-2`}>
         <img
           className={styles.photos}
-          src={photoOne}
+          src={photoTwoUrl}
           alt="Second slide"
         />
-
         <Carousel.Caption>
           <h3>{reviewName}</h3>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         </Carousel.Caption>
       </Carousel.Item>
-      <Carousel.Item>
+
+      <Carousel.Item key={`${photoThree}-3`}>
         <img
           className="d-block w-100"
-          src="holder.js/800x400?text=Third slide&bg=20232a"
+          src={photoThreeUrl}
           alt="Third slide"
         />
 
