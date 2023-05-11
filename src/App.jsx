efff806db2a4053549e83eebde3a045769eeabff
Saoirse Siobhan Ebert <91578619+itsgivingchaotica@users.noreply.github.com>
2023-05-11 10:23:55 -0400

    add left margin search bar

--- src/App.jsx
+++ src/App.jsx
@@ -204,8 +204,8 @@ const handleSearchEngine = () => {
   return (
     <div>
       <Navbar setFilteredResults={setFilteredResults} setSummaryBanner={setSummaryBanner} setDaNeighborhood={setDaNeighborhood} setSearchTerm={setSearchTerm}/>
-      <div>
-        <div className="formatSearch">
+      <div className="formatSearch">
+        <div>
             <SearchBar filterType={filterType} setFilterType={setFilterType} neighborhood={daneighborhood} setNeighborhood={setNeighborhood} handleSearchEngine={handleSearchEngine} zipCodes={zipCodes} setZipCodes={setZipCodes} zip={zip} setZip={setZip} userResult={userResult} searchTerm={searchTerm} setSearchTerm={setSearchTerm} filteredResults={filteredResults} setFilteredResults={setFilteredResults} options={options} results={results} setSummaryBanner={setSummaryBanner} setLocationInquiry={setLocationInquiry} setNeighborhoodList={setNeighborhoodList} neighborhoodList={neighborhoodList}/>
           </div>
           {}
