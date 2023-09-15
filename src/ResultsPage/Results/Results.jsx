1ed60e9866866af75a6898dfac608b9bc840a8ef
Saoirse Siobhan Ebert <91578619+itsgivingchaotica@users.noreply.github.com>
2023-09-15 18:44:49 -0400

    result default export

--- src/ResultsPage/Results/Results.jsx
+++ src/ResultsPage/Results/Results.jsx
@@ -1,17 +1,50 @@
-import React, { useState, useEffect } from 'react';
-import '@fortawesome/fontawesome-free/css/all.min.css';
+import React, { useState, useEffect } from "react";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 import styles from "./results.module.css";
-import { Result } from "../../components/result/Result.jsx";
 import { MapLoader } from "../../components/map/MapLoader.jsx";
-import { Banner } from '../../components/banner/Banner.jsx'
+import { Banner } from "../../components/banner/Banner.jsx";
+import Result from "../../components/result/Result";
 
+export function Results({
+  zip,
+  daneighborhood,
+  userResult,
+  results,
+  filteredResults,
+  business,
+  searchTerm,
+  setSummaryBanner,
+  reviewName,
+  setReviewName,
+  handleSetName,
+  setZipcode,
+  setPhoto1,
+  setPhoto2,
+  setPhoto3,
+  setAltText1,
+  setAltText2,
+  setAltText3,
+  setAddress1,
+  setAddress2,
+  setWebsite,
+  setTelephone,
+  setNeighborhood,
+  setTag,
+  setBlurb1,
+  setBlurb2,
+  setBlurb3,
+  setAbout,
+  reviewLatitude,
+  reviewLongitude,
+  setReviewLatitude,
+  setReviewLongitude,
+  summaryBanner,
+  locationInquiry,
+}) {
+  const [alias, setAlias] = useState("");
+  const [withTop, setWithTop] = useState(150);
 
-export function Results({zip,daneighborhood,userResult,results,filteredResults,business,searchTerm,setSummaryBanner,reviewName,setReviewName,handleSetName,setZipcode,setPhoto1,setPhoto2,setPhoto3,setAltText1,setAltText2,setAltText3,setAddress1,setAddress2,setWebsite,setTelephone,setNeighborhood,setTag,setBlurb1,setBlurb2,setBlurb3,setAbout,reviewLatitude,reviewLongitude,setReviewLatitude,setReviewLongitude,summaryBanner,locationInquiry}){
-
-const [alias,setAlias] = useState("");
-const [withTop, setWithTop] = useState(150);
-
-useEffect(() => {
+  useEffect(() => {
     function handleScroll() {
       if (window.scrollY > 50) {
         setWithTop(80); // update top attribute
@@ -23,36 +56,113 @@ useEffect(() => {
     return () => {
       window.removeEventListener("scroll", handleScroll); // remove event listener
     };
-  }, []); 
+  }, []);
 
-    return (
-        <div>
-            <div>
-                <div className={styles.mapped}>
-                <div>
-                {filteredResults == "" ? (<div className="adjustBanner">
-              
-          <Banner summaryBanner={summaryBanner} searchTerm={searchTerm} locationInquiry={locationInquiry} numberResults={results.length} zip={zip} daneighborhood={daneighborhood}/>
-          </div>):(<div className="adjustBanner">
-              
-          <Banner summaryBanner={summaryBanner} searchTerm={searchTerm} locationInquiry={locationInquiry} numberResults={filteredResults.length} zip={zip} daneighborhood={daneighborhood}/>
-          </div>)}  
-                
-          <br></br>
-                    {filteredResults == "" ? (results.map(result => (
-                            <Result  className={styles.place} key={result.id} result={result} setAlias={setAlias} alias={alias} reviewName={reviewName} setReviewName={setReviewName} handleSetName={handleSetName} setZipcode={setZipcode} setPhoto1={setPhoto1} setPhoto2={setPhoto2} setPhoto3={setPhoto3} setAltText1={setAltText1} setAltText2={setAltText2} setAltText3={setAltText3}
-                            setAddress1={setAddress1} setAddress2={setAddress2} setWebsite={setWebsite} setTelephone={setTelephone} setNeighborhood={setNeighborhood} setTag={setTag} setBlurb1={setBlurb1} setBlurb2={setBlurb2} setBlurb3={setBlurb3} setAbout={setAbout}
-                            setSummaryBanner={setSummaryBanner} setReviewLatitude={setReviewLatitude} setReviewLongitude={setReviewLongitude}/>
-                    ))) :(
-                    filteredResults.map(result => (
-                            <Result className={styles.place} key={result.id} result={result} setAlias={setAlias} alias={alias} reviewName={reviewName} setReviewName={setReviewName} handleSetName={handleSetName} setZipcode={setZipcode} setPhoto1={setPhoto1} setPhoto2={setPhoto2} setPhoto3={setPhoto3} setAltText1={setAltText1} setAltText2={setAltText2} setAltText3={setAltText3}
-                            setAddress1={setAddress1} setAddress2={setAddress2} setWebsite={setWebsite} setTelephone={setTelephone} setNeighborhood={setNeighborhood} setTag={setTag} setBlurb1={setBlurb1} setBlurb2={setBlurb2} setBlurb3={setBlurb3} setAbout={setAbout}
-                            setSummaryBanner={setSummaryBanner} setReviewLatitude={setReviewLatitude} setReviewLongitude={setReviewLongitude}/>
-                    )))}
-                </div>
-                <div className={styles.width} style={{ top: `${withTop}px` }}><MapLoader latitude={reviewLatitude} longitude={reviewLongitude} focus={12}/></div>
-                </div>
-            </div>
-       </div>
-    )
+  return (
+    <div>
+      <div>
+        <div className={styles.mapped}>
+          <div>
+            {filteredResults == "" ? (
+              <div className="adjustBanner">
+                <Banner
+                  summaryBanner={summaryBanner}
+                  searchTerm={searchTerm}
+                  locationInquiry={locationInquiry}
+                  numberResults={results.length}
+                  zip={zip}
+                  daneighborhood={daneighborhood}
+                />
+              </div>
+            ) : (
+              <div className="adjustBanner">
+                <Banner
+                  summaryBanner={summaryBanner}
+                  searchTerm={searchTerm}
+                  locationInquiry={locationInquiry}
+                  numberResults={filteredResults.length}
+                  zip={zip}
+                  daneighborhood={daneighborhood}
+                />
+              </div>
+            )}
+
+            <br></br>
+            {filteredResults == ""
+              ? results.map((result) => (
+                  <Result
+                    className={styles.place}
+                    key={result.id}
+                    result={result}
+                    setAlias={setAlias}
+                    alias={alias}
+                    reviewName={reviewName}
+                    setReviewName={setReviewName}
+                    handleSetName={handleSetName}
+                    setZipcode={setZipcode}
+                    setPhoto1={setPhoto1}
+                    setPhoto2={setPhoto2}
+                    setPhoto3={setPhoto3}
+                    setAltText1={setAltText1}
+                    setAltText2={setAltText2}
+                    setAltText3={setAltText3}
+                    setAddress1={setAddress1}
+                    setAddress2={setAddress2}
+                    setWebsite={setWebsite}
+                    setTelephone={setTelephone}
+                    setNeighborhood={setNeighborhood}
+                    setTag={setTag}
+                    setBlurb1={setBlurb1}
+                    setBlurb2={setBlurb2}
+                    setBlurb3={setBlurb3}
+                    setAbout={setAbout}
+                    setSummaryBanner={setSummaryBanner}
+                    setReviewLatitude={setReviewLatitude}
+                    setReviewLongitude={setReviewLongitude}
+                  />
+                ))
+              : filteredResults.map((result) => (
+                  <Result
+                    className={styles.place}
+                    key={result.id}
+                    result={result}
+                    setAlias={setAlias}
+                    alias={alias}
+                    reviewName={reviewName}
+                    setReviewName={setReviewName}
+                    handleSetName={handleSetName}
+                    setZipcode={setZipcode}
+                    setPhoto1={setPhoto1}
+                    setPhoto2={setPhoto2}
+                    setPhoto3={setPhoto3}
+                    setAltText1={setAltText1}
+                    setAltText2={setAltText2}
+                    setAltText3={setAltText3}
+                    setAddress1={setAddress1}
+                    setAddress2={setAddress2}
+                    setWebsite={setWebsite}
+                    setTelephone={setTelephone}
+                    setNeighborhood={setNeighborhood}
+                    setTag={setTag}
+                    setBlurb1={setBlurb1}
+                    setBlurb2={setBlurb2}
+                    setBlurb3={setBlurb3}
+                    setAbout={setAbout}
+                    setSummaryBanner={setSummaryBanner}
+                    setReviewLatitude={setReviewLatitude}
+                    setReviewLongitude={setReviewLongitude}
+                  />
+                ))}
+          </div>
+          <div className={styles.width} style={{ top: `${withTop}px` }}>
+            <MapLoader
+              latitude={reviewLatitude}
+              longitude={reviewLongitude}
+              focus={12}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/components/Result/Result.jsx
+++ src/components/Result/Result.jsx
@@ -1,85 +1,183 @@
-import React, { useState, useEffect } from 'react';
-import styles from './result.module.css';
-import { Card, OverlayTrigger,Tooltip } from 'react-bootstrap'
-import Placeholder from 'react-bootstrap/Placeholder';
-import { HashRouter as Router, Link, Routes, Route} from 'react-router-dom'
-import { LocationRating } from '../rating/Rating.jsx'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faAnglesRight } from '@fortawesome/free-solid-svg-icons'
+import React, { useState, useEffect } from "react";
+import styles from "./result.module.css";
+import { Card, OverlayTrigger, Tooltip } from "react-bootstrap";
+import Placeholder from "react-bootstrap/Placeholder";
+import { HashRouter as Router, Link, Routes, Route } from "react-router-dom";
+import { LocationRating } from "../rating/Rating.jsx";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAnglesRight } from "@fortawesome/free-solid-svg-icons";
 
-export function Result({result, results, reviewName, setReviewName,setZipcode,setPhoto1,setPhoto2,setPhoto3,setAltText1,setAltText2,setAltText3,setAddress1,setAddress2,setWebsite,setTelephone,setNeighborhood,setTag,setBlurb1,setBlurb2,setBlurb3,setAbout, setSummaryBanner,setReviewLatitude,setReviewLongitude}){
-    const [isLoading, setIsLoading] = useState(true);
+export default function Result({
+  result,
+  results,
+  reviewName,
+  setReviewName,
+  setZipcode,
+  setPhoto1,
+  setPhoto2,
+  setPhoto3,
+  setAltText1,
+  setAltText2,
+  setAltText3,
+  setAddress1,
+  setAddress2,
+  setWebsite,
+  setTelephone,
+  setNeighborhood,
+  setTag,
+  setBlurb1,
+  setBlurb2,
+  setBlurb3,
+  setAbout,
+  setSummaryBanner,
+  setReviewLatitude,
+  setReviewLongitude,
+}) {
+  const [isLoading, setIsLoading] = useState(true);
 
-    useEffect(() => {
-        setTimeout(() => {
-            setIsLoading(false);
-        }, 2000); // set the delay time to 2 seconds
-    }, []);
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 2000); // set the delay time to 2 seconds
+  }, []);
 
-    const renderTooltip = (props) => (
+  const renderTooltip = (props) => (
     <Tooltip id="button-tooltip" {...props}>
       Go to details
     </Tooltip>
   );
 
-    // if isLoading is true, render the skeleton card
-    if (isLoading) {
-        return (
-            <div className={styles.result}>
-                <Placeholder as={Card.Img} variant="top" style={{ height: '200px', width: '200px'}} className={styles['result-image']}/>
-                <Card border="secondary" style={{ width: '600px', height: '200px', display:'flex', marginLeft: '25px', marginRight: '25px' }} className={styles.address}>
-                    <Card.Body>
-                        <Placeholder as={Card.Title} animation="glow" style={{ height: '200px', width:'600px' }}>
-                        <Placeholder xs={6} />
-      <Placeholder className="w-75" /> <Placeholder style={{ width: '25%' }} />
-                        </Placeholder>
-                        <Placeholder as={Card.Text} animation="glow" style={{ height: '20px' }}>
-                            <Placeholder s={8} />
-                        </Placeholder>
-                        <Placeholder as={Card.Text} animation="glow" style={{ height: '20px' }}>
-                            <Placeholder m={6} />
-                        </Placeholder>
-                        <Placeholder as={Card.Text} animation="glow" style={{ height: '10px' }}>
-                            <Placeholder l={6} />
-                        </Placeholder>
-                    </Card.Body>
-                </Card>
-            </div>
-        );
-    
-    }
+  // if isLoading is true, render the skeleton card
+  if (isLoading) {
+    return (
+      <div className={styles.result}>
+        <Placeholder
+          as={Card.Img}
+          variant="top"
+          style={{ height: "200px", width: "200px" }}
+          className={styles["result-image"]}
+        />
+        <Card
+          border="secondary"
+          style={{
+            width: "600px",
+            height: "200px",
+            display: "flex",
+            marginLeft: "25px",
+            marginRight: "25px",
+          }}
+          className={styles.address}
+        >
+          <Card.Body>
+            <Placeholder
+              as={Card.Title}
+              animation="glow"
+              style={{ height: "200px", width: "600px" }}
+            >
+              <Placeholder xs={6} />
+              <Placeholder className="w-75" />{" "}
+              <Placeholder style={{ width: "25%" }} />
+            </Placeholder>
+            <Placeholder
+              as={Card.Text}
+              animation="glow"
+              style={{ height: "20px" }}
+            >
+              <Placeholder s={8} />
+            </Placeholder>
+            <Placeholder
+              as={Card.Text}
+              animation="glow"
+              style={{ height: "20px" }}
+            >
+              <Placeholder m={6} />
+            </Placeholder>
+            <Placeholder
+              as={Card.Text}
+              animation="glow"
+              style={{ height: "10px" }}
+            >
+              <Placeholder l={6} />
+            </Placeholder>
+          </Card.Body>
+        </Card>
+      </div>
+    );
+  }
 
-    const handleSetName = (nombre,zip,photo1,photo2,photo3,altText1,altText2,altText3,latitude,longitude,Address1,Address2,website,telephone,neighborhood,tag,blurb1,blurb2,blurb3,about) => {
-        setReviewName(nombre);
-        console.log(nombre + " " + photo1 + " " + zip + " is hte handledrs")
-        setZipcode(zip);
-        setPhoto1(photo1);
-        setPhoto2(photo2);
-        setPhoto3(photo3);
-        setAltText1(altText1);
-        setAltText2(altText2);
-        setAltText3(altText3);
-        setAddress1(Address1);
-        setAddress2(Address2);
-        setWebsite(website);
-        setTelephone(telephone);
-        setNeighborhood(neighborhood);
-        setTag(tag);
-        setBlurb1(blurb1);
-        setBlurb2(blurb2);
-        setBlurb3(blurb3);
-        setAbout(about);
-        setReviewLatitude(latitude);
-        setReviewLongitude(longitude);
-        setSummaryBanner(nombre);
-    }
-    const handleCardClick = (latitude,longitude) => {
-        setReviewLatitude(latitude);
-        setReviewLongitude(longitude)
-    }
-    const handleSetNameWrapper = (event) => {
-    const { nombre, zip, photo1, photo2, photo3, altText1, altText2, altText3, latitude, longitude, Address1, Address2, website, telephone, neighborhood, tag, blurb1, blurb2, blurb3, about } = result;
-    if (event.type === 'click') {
+  const handleSetName = (
+    nombre,
+    zip,
+    photo1,
+    photo2,
+    photo3,
+    altText1,
+    altText2,
+    altText3,
+    latitude,
+    longitude,
+    Address1,
+    Address2,
+    website,
+    telephone,
+    neighborhood,
+    tag,
+    blurb1,
+    blurb2,
+    blurb3,
+    about
+  ) => {
+    setReviewName(nombre);
+    console.log(nombre + " " + photo1 + " " + zip + " is hte handledrs");
+    setZipcode(zip);
+    setPhoto1(photo1);
+    setPhoto2(photo2);
+    setPhoto3(photo3);
+    setAltText1(altText1);
+    setAltText2(altText2);
+    setAltText3(altText3);
+    setAddress1(Address1);
+    setAddress2(Address2);
+    setWebsite(website);
+    setTelephone(telephone);
+    setNeighborhood(neighborhood);
+    setTag(tag);
+    setBlurb1(blurb1);
+    setBlurb2(blurb2);
+    setBlurb3(blurb3);
+    setAbout(about);
+    setReviewLatitude(latitude);
+    setReviewLongitude(longitude);
+    setSummaryBanner(nombre);
+  };
+  const handleCardClick = (latitude, longitude) => {
+    setReviewLatitude(latitude);
+    setReviewLongitude(longitude);
+  };
+  const handleSetNameWrapper = (event) => {
+    const {
+      nombre,
+      zip,
+      photo1,
+      photo2,
+      photo3,
+      altText1,
+      altText2,
+      altText3,
+      latitude,
+      longitude,
+      Address1,
+      Address2,
+      website,
+      telephone,
+      neighborhood,
+      tag,
+      blurb1,
+      blurb2,
+      blurb3,
+      about,
+    } = result;
+    if (event.type === "click") {
       handleSetName(
         nombre,
         zip,
@@ -102,8 +200,9 @@ export function Result({result, results, reviewName, setReviewName,setZipcode,se
         blurb3,
         about
       );
-    } else if (event.type === 'keydown') {
-      if (event.keyCode === 13) { // Enter key
+    } else if (event.type === "keydown") {
+      if (event.keyCode === 13) {
+        // Enter key
         handleSetName(
           nombre,
           zip,
@@ -129,32 +228,70 @@ export function Result({result, results, reviewName, setReviewName,setZipcode,se
       }
     }
   };
-    
 
-    return (
-        <div className = {styles.result}>
-            <Link to={`/results/${result.id}`} key={result.id}>
-            <img src={result.photo1} alt={`${result.altText1} clickable result image`} className={styles['result-image']} onClick={handleSetNameWrapper} onKeyDown={handleSetNameWrapper}></img></Link>
-                <div className={styles.info} onClick={() => handleCardClick(result.latitude,result.longitude)}>
-                     <Card className={ styles.address }>
-                        <Card.Header>
-                            <LocationRating rating={result.rating}/>
-                        </Card.Header>
-                                <Card.Body>
-                                    <Card.Title className ={styles.share}> {result.name} <br/> <Link to={`/results/${result.id}`} onClick={() => handleSetName(result.name,result.zipcode,result.photo1,result.photo2,result.photo3,result.altText1,result.altText2,result.altText3,result.latitude,result.longitude,result.Address1,result.Address2,result.website,result.telephone,result.neighborhood,result.tag,result.blurb1,result.blurb2,result.blurb3,result.about)}>
-                                     <OverlayTrigger
-                                        placement="right"
-                                        delay={{ show: 250, hide: 400 }}
-                                        overlay={renderTooltip}
-                                    ><FontAwesomeIcon icon={faAnglesRight}/></OverlayTrigger>
-                                    </Link>
-                                    </Card.Title>
-                                <Card.Text>
-                                {result.neighborhood}
-                                </Card.Text>
-                                </Card.Body>
-                    </Card>
-                </div>
-        </div>
-    )
+  return (
+    <div className={styles.result}>
+      <Link to={`/results/${result.id}`} key={result.id}>
+        <img
+          src={result.photo1}
+          alt={`${result.altText1} clickable result image`}
+          className={styles["result-image"]}
+          onClick={handleSetNameWrapper}
+          onKeyDown={handleSetNameWrapper}
+        ></img>
+      </Link>
+      <div
+        className={styles.info}
+        onClick={() => handleCardClick(result.latitude, result.longitude)}
+      >
+        <Card className={styles.address}>
+          <Card.Header>
+            <LocationRating rating={result.rating} />
+          </Card.Header>
+          <Card.Body>
+            <Card.Title className={styles.share}>
+              {" "}
+              {result.name} <br />{" "}
+              <Link
+                to={`/results/${result.id}`}
+                onClick={() =>
+                  handleSetName(
+                    result.name,
+                    result.zipcode,
+                    result.photo1,
+                    result.photo2,
+                    result.photo3,
+                    result.altText1,
+                    result.altText2,
+                    result.altText3,
+                    result.latitude,
+                    result.longitude,
+                    result.Address1,
+                    result.Address2,
+                    result.website,
+                    result.telephone,
+                    result.neighborhood,
+                    result.tag,
+                    result.blurb1,
+                    result.blurb2,
+                    result.blurb3,
+                    result.about
+                  )
+                }
+              >
+                <OverlayTrigger
+                  placement="right"
+                  delay={{ show: 250, hide: 400 }}
+                  overlay={renderTooltip}
+                >
+                  <FontAwesomeIcon icon={faAnglesRight} />
+                </OverlayTrigger>
+              </Link>
+            </Card.Title>
+            <Card.Text>{result.neighborhood}</Card.Text>
+          </Card.Body>
+        </Card>
+      </div>
+    </div>
+  );
 }
